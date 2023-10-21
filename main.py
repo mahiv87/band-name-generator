3d04e35d3da1a4d2a453f7e30569064d69347c0c
mahiv87 <maherreraiv1987@gmail.com>
2023-10-20 23:37:43 -0600

    added generated band name variable

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Welcome to the Band Name Generator!")
 user_city = input("What's the name of the city you grew up in?")
 
 user_pet = input("What's your pet's name?")
+
+generated_band_name = "Your band name could be " + user_city + user_pet
+
