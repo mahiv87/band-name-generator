32e9846328a7c4bf58764ec331e3b6f587ee2cdf
mahiv87 <maherreraiv1987@gmail.com>
2023-10-20 23:39:06 -0600

    debugged spacing in log. print to console

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ user_city = input("What's the name of the city you grew up in?")
 
 user_pet = input("What's your pet's name?")
 
-generated_band_name = "Your band name could be " + user_city + user_pet
+generated_band_name = "Your band name could be " + user_city + " " + user_pet
 
+print(generated_band_name)
